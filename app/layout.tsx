3a69a3beb3482f9fdd3e58a20a6dd4b62b639241
akadinso <akadinso.osuagwu@gmail.com>
2026-04-28 11:46:53 -0400

    Adding global styles and Google Tag Manager integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import { GoogleTagManager } from '@next/third-parties/google'
+import "./globals.css"
 
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
